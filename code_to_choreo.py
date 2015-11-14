b4a02269c9f87f6c680060b953f3e6e036c6a512
ohrely <rebaely@gmail.com>
2015-11-13 16:44:37 -0800

    Wrote in exception cases for swings, stars, and passthrus.  Need more testing.

--- code_to_choreo.py
+++ code_to_choreo.py
@@ -11,11 +11,34 @@ def make_dance():
 
 def simple_trans():
     dance, da_dict = make_dance()
+    # translation = [[], [], [], []]
     translation = []
 
-    for move in dance:
-        move_name = da_dict[move].name
+    i = 0
+    beats = 0
+
+    while i < len(dance):
+        if da_dict[dance[i]].type == da_dict[dance[i - 1]].type:
+            if da_dict[dance[i]].type == "swing":
+                move_name = ""
+            elif da_dict[dance[i]].type == "star":
+                if da_dict[dance[i + 1]].type == "star":
+                    move_name = ""
+                else:
+                    total_moved = 0
+                    k = i - 1
+                    while k > 0:
+                        if da_dict[dance[k]].type == "star":
+                            total_moved += 1
+                        else:
+                            k = 0
+                    move_name = "{} places".format(total_moved)
+            elif da_dict[dance[i]].type == "thru":
+                move_name = da_dict[dance[i]].name + " again"
+        else:
+            move_name = da_dict[dance[i]].name
         translation.append(move_name)
+        beats = beats + da_dict[dance[i]].length
 
     print translation
     return translation
